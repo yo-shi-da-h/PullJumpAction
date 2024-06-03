c58566c1efdedd3fd07b05e2c25a9e8180740d4a
LC1D_ヨシダ_ハルト <146914696+yo-shi-da-h@users.noreply.github.com>
2024-06-03 13:07:19 +0900

    プロジェクト ファイルを追加します。

---
+++ Assets/AroowDraw.cs
@@ -0,0 +1,41 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class AroowDraw : MonoBehaviour
+{
+    [SerializeField]
+    private Image arrowImage;
+    private Vector3 clickPosition;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetMouseButtonDown(0))
+        {
+            clickPosition = Input.mousePosition;
+        }
+        if (Input.GetMouseButton(0))
+        {
+            Vector3 dist = clickPosition - Input.mousePosition;
+
+            float size = dist.magnitude;
+
+            float angleRad = Mathf.Atan2(dist.y, dist.x);
+
+            arrowImage.rectTransform.position = clickPosition;
+
+            arrowImage.rectTransform.rotation
+                = Quaternion.Euler(0, 0, angleRad * Mathf.Rad2Deg);
+
+            arrowImage.rectTransform.sizeDelta = new Vector2(size, size);
+            Debug.Log(dist);
+        }
+    }
+}
---
+++ Assets/PullingJump.cs
@@ -0,0 +1,48 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PullingJump : MonoBehaviour
+{
+    private Rigidbody rb;
+    private Vector3 clickPosition;
+    [SerializeField]
+    private float jumpPower = 10;
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        Debug.Log("è’ìÀÇµÇΩ");
+    }
+    private void OnCollisionStay(Collision collision)
+    {
+        //Debug.Log("ê⁄êGíÜ");
+        isCanJump = true;
+    }
+    private void OnCollisionExit(Collision collision)
+    {
+        //Debug.Log("ó£íEÇµÇΩ");
+        isCanJump = false;
+    }
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb = gameObject.GetComponent<Rigidbody>();
+    }
+
+    // Update is called once per frame
+    private bool isCanJump;
+    void Update()
+    {
+        if (Input.GetMouseButtonDown(0)){
+            clickPosition = Input.mousePosition;
+        }
+        if (isCanJump && Input.GetMouseButtonUp(0))
+        {
+            Vector3 dist = clickPosition - Input.mousePosition;
+
+            if (dist.sqrMagnitude == 0) { return; }
+
+            rb.velocity = dist.normalized * jumpPower;
+        }
+    }
+}
